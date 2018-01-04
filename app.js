159ce6f9570d4ce069b64e9706314043471020c2
Rawle Juglal <rawledjuglal@gmail.com>
2018-01-03 19:32:15 -0600

    added static route to vendor css/js

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(__dirname + '/build/vendor'));
+
 
 app.use('/', index);
 app.use('/users', users);
