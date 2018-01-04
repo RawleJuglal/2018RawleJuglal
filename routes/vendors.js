c34b86c02aed4305e10a9c2031118193716db922
Rawle Juglal <rawledjuglal@gmail.com>
2018-01-03 21:02:36 -0600

    trying to find absolute path to vendor

--- routes/vendors.js
+++ routes/vendors.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('build/vendor/index.html', function(err){
+  res.sendFile('/app/build/vendor/index.html', function(err){
   	if(err){
   		next(err);
   	}
