4300696410eac492589d70691f8f133a1fb51fa4
Rawle Juglal <rawledjuglal@gmail.com>
2018-01-03 19:53:02 -0600

    adding routes for vendor

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var vendors = require('./routes/vendors');
 
 var app = express();
 
@@ -21,11 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/lib', express.static(__dirname + '/build/vendor'));
+
 
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/lib', vendors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/vendors.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ res.json({message: 'You have reached the vendors'});
+});
+
+router.get('/bootstrapCSS', function(req, res, next) {
+  var options = {
+    root: __dirname + '/build/vendor/bootstrap/dist/css',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent':true
+    }
+  };
+
+  res.sendFile('bootstrap.min.css', options, function(err){
+    if(err){
+      next(err);
+    }
+  });
+});
+
+router.get('/jquery', function(req, res, next) {
+  var options = {
+    root: __dirname + '/build/vendor/jquery/dist',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent':true
+    }
+  };
+
+  res.sendFile('jquery.slim.min.js', options, function(err){
+    if(err){
+      next(err);
+    }
+  });
+});
+
+  router.get('/bootstrapJS', function(req, res, next) {
+    var options = {
+      root: __dirname + '/build/vendor/bootstrap/dist/js',
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent':true
+      }
+    };
+
+    res.sendFile('bootstrap.min.js', options, function(err){
+      if(err){
+        next(err);
+      }
+    });
+  });
+
+module.exports = router;
