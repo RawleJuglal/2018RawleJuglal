05cf3bcf99942637a32d99f7a6281d1e42e398ba
Rawle Juglal <rawledjuglal@gmail.com>
2018-01-03 21:00:52 -0600

    trying without sendFile options

--- routes/vendors.js
+++ routes/vendors.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('build/vendor/index.html', { root: __dirname}, function(err){
+  res.sendFile('build/vendor/index.html', function(err){
   	if(err){
   		next(err);
   	}
